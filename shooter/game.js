57eaabc57de78a2208bd5e8d59460d5f8d019e49
Samuel Lucas <smlucas13@gmail.com>
2015-07-25 11:54:43 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -5,19 +5,31 @@ var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
 
 function preload() {
   game.stage.backgroundColor = '#FFFFFF';
-  game.load.image('RCB', 'RCB.gif');
+  game.load.image('submarine', 'submarine.gif');
 
 
 }
 
-var RCB;
+var submarine;
+var cursors;
 
 function create() {
-  RCB = game.add.sprite(400,550,'RCB');
-
+  submarine = game.add.sprite(744,300,'submarine');
+  game.physics.enable(submarine, Phaser.Physics.ARCADE);
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
+  submarine.body.velocity.x = 0;
+  submarine.body.velocity.y = 0;
+  if ( cursors.up.isDown) {
+    submarine.body.velocity.y = -300;
+
+  }
+  else if (cursors.down.isDown) {
+    submarine.body.velocity.y = 300;
+    
+  }
 }
 
 function render() {
