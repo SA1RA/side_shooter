0d715da1340c4b3e028b7ef5dc80cdd97049b3aa
Samuel Lucas <smlucas13@gmail.com>
2015-08-01 11:57:45 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -1,19 +1,26 @@
 'use strict';
 
+var submarine;
+var cursors;
+var backround;
+var ltorpedo;
+
 var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render,});
 
 function preload() {
-  game.stage.backgroundColor = '#FFFFFF';
+  game.load.image('backround', 'backround.gif');
   game.load.image('submarine', 'submarine.gif');
-
+  game.load.image('rtorpedo', 'rtorpedo.gif');
+  game.load.image('ltorpedo', 'ltorpedo.gif');
 
 }
 
-var submarine;
-var cursors;
-
 function create() {
+  backround = game.add.tileSprite(0,0,800,600,'backround');
+  backround.autoScroll(100,0);
+  cursors = game.input.keyboard.createCursorKeys();
+  ltorpedo = game.add.sprite(744,300,'ltorpedo');
   submarine = game.add.sprite(744,300,'submarine');
   game.physics.enable(submarine, Phaser.Physics.ARCADE);
   cursors = game.input.keyboard.createCursorKeys();
@@ -22,13 +29,18 @@ function create() {
 function update() {
   submarine.body.velocity.x = 0;
   submarine.body.velocity.y = 0;
-  if ( cursors.up.isDown) {
-    submarine.body.velocity.y = -300;
 
+  ltorpedo.x = submarine.x;
+  ltorpedo.y = submarine.y;
+
+  if (cursors.up.isUp) {
+    submarine.body.velocity.x = -300;
   }
   else if (cursors.down.isDown) {
     submarine.body.velocity.y = 300;
-    
+}  
+if (game.input.keyboard.isDown(Phaser.Keyboard.SPASEBAR)) {
+
   }
 }
 
