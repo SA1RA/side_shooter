7b106c06ee7877d211b06331b063076b0923962d
Samuel Lucas <smlucas13@gmail.com>
2015-08-15 11:27:09 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -4,6 +4,7 @@ var submarine;
 var cursors;
 var backround;
 var ltorpedo;
+var mine;
 
 var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render,});
@@ -13,7 +14,7 @@ function preload() {
   game.load.image('submarine', 'submarine.gif');
   game.load.image('rtorpedo', 'rtorpedo.gif');
   game.load.image('ltorpedo', 'ltorpedo.gif');
-
+  game.load.image('mine', 'mine.gif');
 }
 
 function create() {
@@ -22,19 +23,22 @@ function create() {
   ltorpedo = game.add.sprite(744,300,'ltorpedo');
   game.physics.enable(ltorpedo, Phaser.Physics.ARCADE);
   submarine = game.add.sprite(744,300,'submarine');
+  mine = game.add.sprite(100, 300, 'mine');
   game.physics.enable(submarine, Phaser.Physics.ARCADE);
   cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
+  game.physics.arcade.overlap(ltorpedo, mine, collisionHandler, null, this);
+
   submarine.body.velocity.x = 0;
   submarine.body.velocity.y = 0;
 
   if (! ltorpedo.shooting) {
     ltorpedo.body.velocity.x = 0;
     ltorpedo.body.velocity.x = 0;
-    ltorpedo.x = submarine.x;
-    ltorpedo.y = submarine.y;
+    ltorpedo.x = submarine.x + 16;
+    ltorpedo.y = submarine.y + 16;
     ltorpedo.renderable = true;
   }
 
@@ -60,4 +64,17 @@ function update() {
 }
 
 function render() {
+
+function collisionHandler(ltorpedo, mine) {
+  ltorpedo.reset();
+  mine.kill();
+}
+function minereset() {
+    ltorpedo.shooting = false;
+    ltorpedo.renderable = false;
+    ltorpedo.body.velocity.x = 0;
+    ltorpedo.body.velocity.y = 0;
+    ltorpedo = submarine.x;
+    ltorpedo = submarine.y;
+}
 }
